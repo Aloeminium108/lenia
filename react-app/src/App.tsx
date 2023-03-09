d87c917f818be3148bbe298002a527d0622ff935
Alice Parrish <aloeasinthevera@gmail.com>
2023-03-08 20:49:39 -0500

    Adjusted styling

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -29,7 +29,7 @@ class App extends React.Component {
 
     render() {
         return (
-            <>
+            <div id='home'>
                 <div className='control-panel'>
                     <KernelControls />
                     <GrowthControls />
@@ -43,7 +43,7 @@ class App extends React.Component {
                     <ResetButton />
                     <ClearButton />
                 </div>
-            </>
+            </div>
         )
     }
 }
